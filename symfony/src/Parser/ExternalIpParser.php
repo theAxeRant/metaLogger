5a276f1cbf63acfc18be46de1a9eedb0e42f2a43
Ryan W. Howe <ryanwhowe@gmail.com>
2025-09-21 16:58:52 -0400

    Added External Ip Check

---
+++ symfony/src/Parser/ExternalIpParser.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Theaxerant\Metalogger\Parser;
+
+use Theaxerant\Metalogger\Logger;
+use Theaxerant\Metalogger\Parser\ParserInterface;
+
+class ExternalIpParser implements ParserInterface {
+
+    protected const SINGLE_LOGGER_KEY = 'IPV4';
+    /**
+     * @var Logger
+     */
+    private $logger;
+
+    public function __construct(Logger $logger) {
+        $this->logger = $logger;
+    }
+
+    private function getExternalIp(): string {
+        $ipv4 = file_get_contents('https://ipv4.icanhazip.com/');
+        return trim($ipv4);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function log(): void {
+        $externalIp = $this->getExternalIp();
+        $this->logger->single(self::SINGLE_LOGGER_KEY, $externalIp);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function debug(): array {
+        $externalIp = $this->getExternalIp();
+        return [
+            "External IPv4 {$externalIp}",
+        ];
+    }
+}
